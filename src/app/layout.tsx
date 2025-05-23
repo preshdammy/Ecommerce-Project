40ede0b6c454d4ab8027bb321c12c82cdaf78c40
Opemipo Shofolahan <opemiposhofolahan@Opemipos-MacBook-Pro.local>
2025-05-23 11:04:11 +0100

    navbar and footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ApolloProvider from "@/shared/provider/apolloProvider";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +31,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ApolloProvider>
+          <Navbar />
           {children}
+          <Footer />
         </ApolloProvider>
         
       </body>
