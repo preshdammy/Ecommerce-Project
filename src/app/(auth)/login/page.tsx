76e6c7c0865cf1cb6fb0aa6ca813441f79b9ad1b
Opemipo Shofolahan <opemiposhofolahan@Opemipos-MacBook-Pro.local>
2025-06-13 10:20:38 +0100

    cart, messages, wishlist, cart and like window in header and privacy policy

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -10,31 +10,31 @@ const Login = () => {
   return (
     <>
         <div className=' bg-[rgba(0,0,0,0.3)] w-full h-screen flex items-center justify-center'>
-            <div className='bg-white w-[583px] p-[50px] flex flex-col border-[1px] border-[#cce5ff]  gap-[25px] items-center text-[#272222] h-[651px] rounded-[32px]'>
-                <h2 className='text-[40px] font-normal text-[#55a7ff]'>Log in</h2>
-                <div className='w-full flex border-[1px] border-[rgba(0,0,0,0.1)] rounded-[16px] py-3 justify-center items-center gap-[15px]'>
-                    <Image src={google} alt=''/>
+            <div className='bg-white lg:w-[583px] lg:p-[50px] flex flex-col border-[1px] border-[#cce5ff]  lg:gap-[25px] items-center text-[#272222] lg:h-[651px] rounded-[32px] sm:w-[304px] w-[304px] sm:h-[458px] h-[458px] sm:p-[20px] p-[20px] sm:gap-[20px] gap-[20px]'>
+                <h2 className='lg:text-[40px] lg:font-normal text-[#55a7ff] sm:text-[24px] text-[24px] sm:font-normal font-normal'>Log in</h2>
+                <div className='w-full flex border-[1px] border-[rgba(0,0,0,0.1)] lg:rounded-[16px] lg:py-3 justify-center items-center lg:gap-[15px] lg:text-[20px] sm:rounded-[100px] rounded-[100px] sm:text-[12px] text-[12px] sm:font-normal font-normal sm:gap-[5px] gap-[5px] sm:py-2 py-2'>
+                    <Image className='sm:w-[16px] w-[16px] sm:h-[16px] h-[16px] lg:w-[24px] lg:h-[24px]' src={google} alt='' />
                     <button>Sign in with Google</button>
                 </div>
-                <p className='text-[16px] font-normal'>OR</p>
-                <div className='flex w-full gap-[20px] flex-col'>
-                    <input className='w-full px-7 py-3 border-[1px] border-[rgba(0,0,0,0.1)] outline-[#4ff072] rounded-[16px]' placeholder='Enter email address'  type="text" />
-                    <div tabIndex={0} className='w-full px-7 py-3 flex justify-between focus-within:outline focus-within:outline-[#4ff072] border-[1px] border-[rgba(0,0,0,0.1)] rounded-[16px]'>
-                    <input className='outline-none w-full' placeholder='Enter password' type={showpassword? "text": "password"} />
+                <p className='lg:text-[16px] lg:font-normal sm:text-[12px] text-[12px] sm:font-[500] font-[500]'>OR</p>
+                <div className='flex w-full lg:gap-[20px] flex-col sm:gap-[10px] gap-[10px]'>
+                    <input className='w-full px-7 lg:py-3 border-[1px] border-[rgba(0,0,0,0.1)] lg:bg-white outline-[#4ff072] lg:rounded-[16px] lg:placeholder:text-[16px] sm:rounded-[100px] rounded-[100px] sm:placeholder:text-[12px] placeholder:text-[12px] sm:placeholder:font-normal placeholder:font-normal sm:bg-[#f8f8f8] bg-[#f8f8f8] sm:py-2 py-2' placeholder='Enter email address'  type="text" />
+                    <div tabIndex={0} className='w-full px-7 lg:py-3 flex justify-between focus-within:outline lg:bg-white focus-within:outline-[#4ff072] border-[1px] border-[rgba(0,0,0,0.1)] lg:rounded-[16px] sm:rounded-[100px] rounded-[100px] sm:bg-[#f8f8f8] bg-[#f8f8f8] sm:py-2 py-2'>
+                    <input className='outline-none w-full lg:placeholder:text-[16px] sm:placeholder:text-[12px] placeholder:text-[12px] sm:placeholder:font-normal placeholder:font-normal' placeholder='Enter password' type={showpassword? "text": "password"} />
                     <button onClick={()=>setshowpassword(prev => !prev)}>
                     {showpassword ? (
-                         <IoEyeOutline size={24} className='opacity-[20%]' />
+                         <IoEyeOutline size={24} className='opacity-[20%] lg:w-[24px] lg:h-[24px] sm:w-[16px] w-[16px] sm:h-[16px] h-[16px]' />
                     ) : (
-                        <Image src={eyeclosed} alt='' />
+                        <Image className='lg:w-[26px] lg:h-[24px] sm:w-[18px] w-[18px] sm:h-[16px] h-[16px]' src={eyeclosed} alt='' />
                     )}
                     </button>
                     </div>
                 </div>
-                <div className='flex w-full gap-[20px] flex-col'>
-                    <button className='w-full py-3 bg-[#007bff] text-white text-[16px] font-normal rounded-[16px]'>Log in</button>
-                    <button className='w-full py-3 border-[1px] border-[#007bff] text-[#007bff] text-[16px] font-normal  rounded-[16px]'>Forgot Password?</button>
+                <div className='flex w-full lg:gap-[20px] flex-col sm:gap-[10px] gap-[10px]'>
+                    <button className='w-full lg:py-3 bg-[#007bff] text-white lg:text-[16px] lg:font-normal lg:rounded-[16px] sm:rounded-[100px] rounded-[100px] sm:text-[12px] text-[12px] sm:font-[600] font-[600] sm:py-2 py-2'>Log in</button>
+                    <button className='w-full lg:py-3 border-[1px] border-[#007bff] text-[#007bff] lg:text-[16px] lg:font-normal  lg:rounded-[16px] sm:rounded-[100px] rounded-[100px] sm:text-[12px] text-[12px] sm:font-normal font-normal sm:py-2 py-2'>Forgot Password?</button>
                 </div>
-            <div className='flex text-[16px] mt-[20px] font-normal gap-2'>
+            <div className='flex lg:text-[16px] lg:mt-[20px] lg:font-normal gap-2 sm:text-[12px] text-[12px] sm:font-normal font-normal sm:mt-[10px] mt-[10px]'>
                 <p>Don't have an account?</p>
                 <a className='text-[#007bff] font-bold'>Sign up</a>
             </div>
--- src/app/(landingpage)/profile-page/page.tsx
+++ src/app/(landingpage)/profile-page/page.tsx
@@ -1,45 +1,66 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import frame from '../../../../public/figma images/Group 40.png'
-import altimage from '../../../../public/figma images/🦆 icon _image outline_.png'
-import colcamera from '../../../../public/figma images/Frame 193.png'
-import ellipse from '../../../../public/figma images/Ellipse 7.png'
+import frame from '../../../../public/figma images/Component 2.png'
 import shop from '../../../../public/figma images/🦆 icon _shop alt_.png'
 
 const Profilepage = () => {
   return (
    <>
-    <div className='px-[40px] py-[50px]'>
-        <div className='flex gap-[30px] items-center'>
-            <div>
-                <Image className='relative' src={frame} alt=''/>
-                <Image className='absolute top-[400px] left-[110px]' src={altimage} alt=''/>
-                <Image className='absolute top-[465px] left-[194px] z-10' src={colcamera} alt=''/>
-            </div>
-            <div className='flex flex-col gap-[10px]'>
-                <h2 className='text-[40px] font-normal'>Bamidele Ogunbiyi</h2>
-                <div className='text-[16px] font-normal text-[#939090] flex gap-[15px]'>
-                    <p>Lagos, Nigeria</p>
-                    <p className='flex gap-[2px]'><Image src={shop} alt=''/>Soothe & Tie</p>
-                </div>
-                <Link className='text-[16px] text-[#939090] font-normal' href=''>Edit personal details</Link>
-            </div>
-        </div>
-        <div className='flex justify-between mt-[120px] mb-[250px]'>
-            <button className='flex border-[1px] focus:text-[#007bff] focus:bg-[#cce5ff] text-[24px] font-normal border-[#cce5ff] gap-[40px] px-8 py-6 rounded-[14px]'>
-                <p>My Purchases</p>
-                <p className='text-[#007bff] font-[600] text-[24px]'>34</p>
-            </button>
-            <button className='flex border-[1px] focus:text-[#007bff] focus:bg-[#cce5ff] text-[24px] font-normal border-[#cce5ff] gap-[40px] px-8 py-6 rounded-[14px]'>
-                <p>My Deliveries</p>
-                <p className='text-[#007bff] font-[600] text-[24px]'>11</p>
-            </button>
-            <button className='flex border-[1px] focus:text-[#007bff] focus:bg-[#cce5ff] text-[24px] font-normal border-[#cce5ff] px-15 py-6 rounded-[14px]'>
-               My Payments
-            </button>
-        </div>
+   <div className='px-[20px] sm:px-[30px] md:px-[40px] xl:px-[60px] py-[40px] sm:py-[50px] md:py-[60px] xl:py-[70px]'>
+  <div className='flex gap-[10px] items-center'>
+    <div>
+      <Image
+        className='w-[80px] h-[80px] sm:w-[100px] sm:h-[100px] md:w-[160px] md:h-[160px] lg:w-[216px] lg:h-[216px] xl:w-[216px] xl:h-[216px]'
+        src={frame}
+        alt=''
+      />
     </div>
+    <div className='flex flex-col gap-[10px]'>
+      <h2 className='text-[16px] font-[500] sm:text-[20px] sm:font-[600] md:text-[32px] md:font-[600] lg:text-[40px] lg:font-normal xl:text-[40px] xl:font-normal'>
+        Bamidele Ogunbiyi
+      </h2>
+      <div className='text-[12px] sm:text-[14px] md:text-[16px] lg:text-[16px] xl:text-[16px] text-[#939090] font-normal flex gap-[15px] mt-[-8px] sm:mt-[-6px] md:mt-0'>
+        <p>Lagos, Nigeria</p>
+        <p className='flex gap-[2px] items-center'>
+          <Image
+            className='w-[15px] h-[15px] sm:w-[16px] sm:h-[16px] md:w-[18px] md:h-[18px]'
+            src={shop}
+            alt=''
+          />
+          Soothe & Tie
+        </p>
+      </div>
+      <Link
+        className='text-[12px] sm:text-[14px] md:text-[16px] xl:text-[16px] text-[#939090] font-normal'
+        href=''
+      >
+        Edit personal details
+      </Link>
+    </div>
+  </div>
+
+  <div className='flex flex-col gap-[20px] mt-[60px] sm:mt-[70px] md:mt-[100px] lg:mt-[120px] xl:mt-[130px] mb-[90px] sm:mb-[100px] md:mb-[200px] lg:mb-[250px] xl:mb-[260px] md:flex-row md:gap-[10%]'>
+    <button className='flex items-center justify-between gap-[20px] px-[16px] sm:px-[20px] md:px-[24px] xl:px-[28px] py-[20px] sm:py-[24px] md:py-[28px] xl:py-[30px] w-[201px] sm:w-[250px] md:w-full text-[12px] sm:text-[14px] md:text-[20px] lg:text-[24px] xl:text-[24px] font-[500] md:font-normal xl:font-normal border-[1px] border-[#cce5ff] rounded-[14px] focus:text-[#007bff] focus:bg-[#f5faff]'>
+      <p>My Purchases</p>
+      <p className='text-[#007bff] font-[500] sm:font-[600] md:font-[600] text-[12px] sm:text-[14px] md:text-[20px] lg:text-[24px] xl:text-[24px]'>
+        34
+      </p>
+    </button>
+
+    <button className='flex items-center justify-between gap-[20px] px-[16px] sm:px-[20px] md:px-[24px] xl:px-[28px] py-[20px] sm:py-[24px] md:py-[28px] xl:py-[30px] w-[201px] sm:w-[250px] md:w-full text-[12px] sm:text-[14px] md:text-[20px] lg:text-[24px] xl:text-[24px] font-[500] md:font-normal xl:font-normal border-[1px] border-[#cce5ff] rounded-[14px] focus:text-[#007bff] focus:bg-[#f5faff]'>
+      <p>My Deliveries</p>
+      <p className='text-[#007bff] font-[500] sm:font-[600] md:font-[600] text-[12px] sm:text-[14px] md:text-[20px] lg:text-[24px] xl:text-[24px]'>
+        11
+      </p>
+    </button>
+
+    <button className='flex items-center justify-center px-[16px] sm:px-[20px] md:px-[24px] xl:px-[28px] py-[20px] sm:py-[24px] md:py-[28px] xl:py-[30px] w-[201px] sm:w-[250px] md:w-full text-[12px] sm:text-[14px] md:text-[20px] lg:text-[24px] xl:text-[24px] font-[500] md:font-normal xl:font-normal border-[1px] border-[#cce5ff] rounded-[14px] focus:text-[#007bff] focus:bg-[#f5faff]'>
+      My Payments
+    </button>
+  </div>
+</div>
+
    </>
   )
 }
