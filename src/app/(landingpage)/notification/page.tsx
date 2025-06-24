8a464b74d63f3bbce4839111bd01d1259ca1b70f
Afoking112 <adewoleafolabi90@gamil.com>
2025-06-24 09:47:27 +0200

    correction on three pages

--- src/app/(landingpage)/notification/page.tsx
+++ src/app/(landingpage)/notification/page.tsx
@@ -8,22 +8,23 @@ const Notification = () => {
     {/* Notifications Section */}
       <div className="max-w-3xl mx-auto  min-h-screen ml-20  ">
         <div className=" pt-30 h-auto ">
-<h2 className="text-xl font-semibold font-Merriweather  text-blue-600 mb-4">My Notifications</h2>
+<h2 className="text-xl font-semibold font-Merriweather text-[#55A7FF]  text-[40px] mb-6">My Notifications</h2>
         <div className="space-y-4">
           
           <div
   className=" w-[816px] h-[120px] flex items-start justify-between px-4 py-3 rounded-[16px] border bg-[#F5FAFF] border-[#CCE5FF] "
 >
 
-              <div className="flex items-start space-x-4">
-                <img className="bg-gray-300 w-10 h-10 rounded-full" src="/figma images/Frame 188.png" />
-                <div>
-                  <p className="text-blue-600 font-medium">Your password has been successfully changed</p>
-                  <p className="text-sm text-gray-500 mt-3">December 19, 2022</p>
+              <div className="flex items-center  space-x-4">
+                <img className="bg-gray-300 w-[100px] h-[100px] mt-[-5px] rounded-full" src="/figma images/Frame 188.png" />
+                <div className="">
+                  <p className="text-[#007BFF] text-[16px] font-medium">Your password has been successfully changed</p>
+                  <p className="text-[#939090] text-[16px] mt-3">December 19, 2022</p>
                 </div>
               </div>
-              <p className="text-sm text-gray-400 mt-17">30 mins ago</p>
+              <p className="text-[16px] text-[#939090] mt-15">30 mins ago</p>
             </div>
+            
         
         </div>
         </div>
--- src/app/(landingpage)/product/page.tsx
+++ src/app/(landingpage)/product/page.tsx
@@ -4,18 +4,21 @@ const ProductUpload = () => {
         <div className="">
 
             <div className="min-h-screen  py-8 ">
-                <div className="w-full mx-auto h-[190vh] bg-gray-100 mb-[-30px]  ">
-                    <h2 className=" font-Merriweather text-2xl font-medium ml-18 text-blue-600 mb-6 ">
+                <div className="w-full mx-auto h-[240vh] bg-gray-100 mb-[-30px]  ">
+                  <div className="pt-[200px] ">
+                    <h2 className=" font-Merriweather text-[40px] w-[325px] h-[50px] font-medium ml-20 text-[#55A7FF] mb-6 ">
   Upload Products
 </h2>
 
-                    <form className="grid grid-cols-1 md:grid-cols-3 px-[80px]">
+                  </div>
+
+                    <form className="grid grid-cols-1 md:grid-cols-3 px-[90px]">
                         {/* Main Image Upload */}
   {/* Main Image Upload */}
- <div className="md:col-span-2">
+ <div className="md:col-span-2 ">
   <label
     htmlFor="mainImage"
-    className="cursor-pointer flex flex-col justify-center items-center  h-[360px] w-[696px] bg-blue-50 relative rounded-[16px]  hover:bg-blue-100 transition-all duration-200"
+    className="cursor-pointer flex flex-col justify-center items-center  h-[55vh] w-[80%] bg-blue-50 relative rounded-[16px]  hover:bg-blue-100 transition-all duration-200"
   >
     <input
       id="mainImage"
@@ -26,30 +29,30 @@ const ProductUpload = () => {
       <img
         src="/figma images/share.png"
         alt=""
-        className="mx-auto h-8 w-8 mb-3"
+        className="mx-auto h-10 w-10 mb-3"
       />
-      <p className="text-black ">Upload Image</p>
+      <p className="text-black text-[20px] ">Upload Image</p>
     </div>
   </label>
 </div>
 
 
              {/* 4 Additional Thumbnails */}
-<div className="grid grid-cols-2 gap-2">
+<div className="grid grid-cols-2 ml-[-150px]">
   {Array.from({ length: 4 }).map((_, idx) => (
     <label
       key={idx}
-      className="h-[152px] w-[184px] rounded-[16px] flex items-center justify-center bg-white cursor-pointer relative"
+      className="h-[190px] w-[280px] rounded-[16px] flex items-center justify-center bg-white cursor-pointer relative"
     >
       <input
         type="file"
-        className="absolute inset-0 opacity-0 cursor-pointer"
+        className="absolute inset-0 opacity-0 cursor-pointer ml-[-100px] "
       />
-      <div className="pointer-events-none text-center">
+      <div className="pointer-events-none text-center  ">
         <img
           src="/figma images/image-03.png"
           alt="Thumbnail placeholder"
-          className="h-6 w-6 mx-auto"
+          className="h-10 w-10 mx-auto "
         />
       </div>
     </label>
@@ -57,23 +60,24 @@ const ProductUpload = () => {
 </div>
 
 
+
                         {/* Input Fields */}
                         <div className=" ml-[-20px]  mt-4 ">
                           <form action="" className="space-y-4">
                          <div className="grid grid-cols-2 gap-4 ml-[30px]">
       <div>
-        <label className=" block text-sm font-medium text-gray-700 mb-1">Product Name</label>
+        <label className=" block text-sm font-medium text-gray-700 mb-1 text-[24px]">Product Name</label>
         <input
           type="text"
-          className="bg-white w-[696px] h-[88px] rounded-[16px] px-4"
+          className="bg-white w-[800px] h-[88px] rounded-[16px] px-4"
           placeholder=""
         />
       </div>
-      <div className="ml-[560px]">
-        <label className="block text-sm font-medium text-gray-700 mb-1 ml-3">Category</label>
+      <div className="ml-[600px]">
+        <label className="block text-sm font-medium text-gray-700 mb-1 ml-3 text-[24px]" >Category</label>
         <input
           type="text"
-          className="bg-white w-[384px] h-[88px] rounded-[16px] px-4"
+          className="bg-white w-[584px] h-[88px] rounded-[16px] px-4"
           placeholder=""
         />
       </div>
@@ -81,38 +85,46 @@ const ProductUpload = () => {
 
      <div className="grid grid-cols-2 gap-4 ml-[30px]">
       <div>
-        <label className="block text-sm font-medium text-gray-700 mb-1">Product Description</label>
+        <label className="block text-sm font-medium text-gray-700 mb-1 text-[24px]">Product Description</label>
         <input
           type="text"
-          className="bg-white w-[696px] h-[176px] rounded-[16px] px-4"
+          className="bg-white w-[800px] h-[176px] rounded-[16px] px-4"
           placeholder=""
           aria-rowspan={3}
         />
       </div>
-      <div className="ml-[560px]">
-        <label className="block text-sm font-medium text-gray-700 mb-1 ml-3 w-[100px]">Sub-category</label>
+      <div className="ml-[600px]">
+        <label className="block text-sm font-medium text-gray-700 mb-1 ml-3 w-[200px] text-[24px]">Sub-category</label>
         <input
           type="text"
-          className="bg-white w-[384px] h-[176px] rounded-[16px] px-4"
+          className="bg-white w-[584px] h-[176px] rounded-[16px] px-4"
           placeholder=""
         />
       </div>
     </div>
                           
-       <div className="grid grid-cols-2 gap-4 ml-[30px]">
-      <div>
-        <label className="block text-sm font-medium text-gray-700 mb-1">Color</label>
+       <div className="grid grid-cols-3 gap-4 ml-[30px]">
+          <div>
+        <label className="block text-sm font-medium text-gray-700 mb-1 text-[24px] w-[200px]">Color</label>
         <input
           type="text"
-          className="bg-white w-[696px] h-[88px] rounded-[16px] px-4"
+          className="bg-white w-[374px] h-[88px] rounded-[16px] px-4"
           placeholder=""
         />
       </div>
-      <div className="ml-[560px]">
-        <label className="block text-sm font-medium text-gray-700 mb-1 ml-3">Condition</label>
+      <div className="ml-[260px]">
+        <label className="block text-sm font-medium text-gray-700 mb-1 ml-3 text-[24px]">stock</label>
+        <input
+          type="number"
+          className="bg-white w-[374px] h-[88px] rounded-[16px] px-4"
+          placeholder=""
+        />
+      </div>
+      <div className="ml-[520px]">
+        <label className="block text-sm font-medium text-gray-700 mb-1 ml-3 text-[24px]">Condition</label>
         <input
           type="text"
-          className="bg-white w-[384px] h-[88px] rounded-[16px] px-4"
+          className="bg-white w-[584px] h-[88px] rounded-[16px] px-4"
           placeholder=""
         />
       </div>
@@ -120,25 +132,25 @@ const ProductUpload = () => {
                             
      <div className="grid grid-cols-3 gap-1 ml-[30px]">
       <div>
-        <label className="block text-sm font-medium text-gray-700 mb-1">Minimum order</label>
+        <label className="block text-sm font-medium text-gray-700 mb-1 text-[24px] w-[200px]">Minimum order</label>
         <input
           type="number"
-          className="bg-white w-[304px] h-[88px] rounded-[16px] px-4"
+          className="bg-white w-[374px] h-[88px] rounded-[16px] px-4"
           placeholder=""
         />
       </div>
-      <div className="ml-[200px]">
-        <label className="block text-sm font-medium text-gray-700 mb-1 ml-3">Price</label>
+      <div className="ml-[260px]">
+        <label className="block text-sm font-medium text-gray-700 mb-1 ml-3 text-[24px]">Price</label>
         <input
           type="number"
-          className="bg-white w-[304px] h-[88px] rounded-[16px] px-4"
+          className="bg-white w-[374px] h-[88px] rounded-[16px] px-4"
           placeholder=""
         />
       </div>
-       <div className="ml-[500px] mt-[16px]">
+       <div className="ml-[530px] mt-[16px]">
   <button
     type="submit"
-    className="w-[385px] h-[92px] rounded-[100px] bg-red-500 text-white pt-[32px] pb-[32px] pr-[100.5px] pl-[100.5px] hover:bg-red-600 transition-all"
+    className="w-[585px] h-[92px] mt-3 rounded-[100px] text-[24px] font-WorkSans font-semibold bg-[#FF4C3B] text-[#FFFFFF] pt-[32px] pb-[32px] pr-[100.5px] pl-[100.5px] hover:bg-red-600 transition-all"
   >
     Upload product
   </button>
