3656260ef848cd1d6eaf3c695fff05ffa0650740
Opemipo Shofolahan <opemiposhofolahan@Opemipos-MacBook-Pro.local>
2025-06-30 13:51:24 +0100

    save before merge

--- src/shared/graphql/schema/vendor/vendortypedef.ts
+++ src/shared/graphql/schema/vendor/vendortypedef.ts
@@ -1,13 +1,4 @@
 export const vendortypedef = `
- type Vendor {
-    id: ID!
-    companyName: String!
-    email: String!
-    role: String!
-  }
-
-  extend type Query {
-    allVendors: [Vendor!]!
-  }
+ 
 
 `
